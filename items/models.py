77e7b8a51192301d12aa7a19e5991c4d4928466d
LopezWellsSebastian <sebastianwells33@hotmail.com>
2022-12-05 20:08:18 -0800

    Update models.py

--- items/models.py
+++ items/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
-
-
 # Create your models here.
 class Type(models.Model):
     name = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
-
-
-
 class Items(models.Model):
     title = models.CharField(max_length=256)
     price = models.DecimalField(decimal_places=2, max_digits= 10)
