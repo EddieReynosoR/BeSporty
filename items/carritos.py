9d0419b476487fade64837dc7947b0964ff25994
EddieReynosoR <eduardo_3reynoso@hotmail.com>
2022-12-15 08:10:14 -0800

    Cart Working

--- items/carritos.py
+++ items/carritos.py
@@ -10,8 +10,10 @@ class Cart:
             self.cart = cart
 
     def add(self, item, size, quantity):
-        id=str(item.id)
-        if id not in self.cart.keys():
+    
+        id=0
+        # print(self.cart)
+        if str(id) not in self.cart.keys():
             self.cart[id] = {
                 "item_id": item.id,
                 "uid": item.id,
@@ -22,37 +24,111 @@ class Cart:
                 "quantity": int(quantity),
                 "size": size,
             }
-        elif int(str(quantity))>1:
-            if self.cart[id]["size"] != size:
-                id = len(self.cart.keys())+1
-                self.cart[id] = {
-                "item_id": id, 
-                "uid": item.id,
-                "name": item.title,
-                 "price": float(item.price),
-                "price_acum": round(float(item.price)*int(quantity),2),
-                "type": item.type.name,
-                "quantity": int(quantity),
-                "size": size,
-            }
-            else:
-                self.cart[id]["quantity"] += int(quantity)
-                self.cart[id]["price_acum"] += float(item.price)*int(quantity)
-                self.cart[id]["price_acum"] = round(self.cart[id]["price_acum"],2)
-        elif self.cart[id]["size"] != size:
-            self.cart[id] = {
-                "item_id": item.id,
-                "name": item.title,
-                "price_acum": round(float(item.price)*int(quantity),2),
-                "type": item.type.name,
-                "quantity": int(quantity),
-                "size": size,
-            }
         else:
-            self.cart[id]["quantity"] +=1
-            self.cart[id]["price_acum"] += float(item.price)
-            self.cart[id]["price_acum"] = round(self.cart[id]["price_acum"],2)
+            if self.cart[str(id)]["item_id"] == item.id and self.cart[str(id)]["size"] == size:
+                    # print(id)
+                    if int(str(quantity))>1:
+                        self.cart[str(id)]["quantity"] += int(quantity)
+                        self.cart[str(id)]["price_acum"] += float(item.price)
+                        self.cart[str(id)]["price_acum"] = round(self.cart[str(id)]["price_acum"])
+                    else:
+                        self.cart[str(id)]["quantity"] +=1
+                        self.cart[str(id)]["price_acum"] += float(item.price)
+                        self.cart[str(id)]["price_acum"] = round(self.cart[str(id)]["price_acum"])
+
+                    # print(self.cart[str(id)])
+            
+            else:
+                while str(id) in self.cart.keys():
+                    if self.cart[str(id)]["item_id"] == item.id and self.cart[str(id)]["size"] == size:
+                        break
+                    else:
+                        id += 1
+                    # print(id)
+                if str(id) not in self.cart.keys():
+                    self.cart[id] = {
+                    "item_id": item.id,
+                    "uid": item.id,
+                    "name": item.title,
+                    "price": float(item.price),
+                    "price_acum": round(float(item.price)*int(quantity),2),
+                    "type": item.type.name,
+                    "quantity": int(quantity),
+                    "size": size,
+                }
+                else:
+                    if self.cart[str(id)]["item_id"] == item.id and self.cart[str(id)]["size"] == size:
+                        # print(id)
+                        if int(str(quantity))>1:
+                            print(id)
+                            self.cart[str(id)]["quantity"] += int(quantity)
+                            self.cart[str(id)]["price_acum"] += float(item.price)
+                            self.cart[str(id)]["price_acum"] = round(self.cart[str(id)]["price_acum"])
+                        else:
+                            self.cart[str(id)]["quantity"] +=1
+                            self.cart[str(id)]["price_acum"] += float(item.price)
+                            self.cart[str(id)]["price_acum"] = round(self.cart[str(id)]["price_acum"])
+
+                        # print(self.cart[str(id-1)]
+            
+            
+            
+            
+
+            # if self.cart[str(id)]["item_id"] == item.id and self.cart[str(id)]["size"] != size:
+            #     self.cart[str(id)] = {
+            #         "item_id": item.id,
+            #         "uid": item.id,
+            #         "name": item.title,
+            #         "price": float(item.price),
+            #         "price_acum": round(float(item.price)*int(quantity),2),
+            #         "type": item.type.name,
+            #         "quantity": int(quantity),
+            #         "size": size,
+            #     }
+
+                
+
+           
+
+
+        # print(self.cart)
         self.saveCart()
+        # id = str(item.id)
+        # if id not in self.cart.keys():
+        #     self.cart[id] = {
+        #         "item_id": item.id,
+        #         "uid": item.id,
+        #         "name": item.title,
+        #         "price": float(item.price),
+        #         "price_acum": round(float(item.price)*int(quantity),2),
+        #         "type": item.type.name,
+        #         "quantity": int(quantity),
+        #         "size": size,
+        #     }
+        # elif int(str(quantity))>1:
+            
+        #     if self.cart[id]["size"] != size:
+        #         id = len(self.cart.keys())+1
+        #         self.cart[id] = {
+        #         "item_id": item.id, 
+        #         "uid": id,
+        #         "name": item.title,
+        #         "price": float(item.price),
+        #         "price_acum": round(float(item.price)*int(quantity),2),
+        #         "type": item.type.name,
+        #         "quantity": int(quantity),
+        #         "size": size,
+        #     }
+        #     else:
+        #         self.cart[id]["quantity"] += int(quantity)
+        #         self.cart[id]["price_acum"] += float(item.price)*int(quantity)
+        #         self.cart[id]["price_acum"] = round(self.cart[id]["price_acum"],2)
+        # else:
+        #     self.cart[id]["quantity"] +=1
+        #     self.cart[id]["price_acum"] += float(item.price)
+        #     self.cart[id]["price_acum"] = round(self.cart[id]["price_acum"],2)
+        # self.saveCart()
 
     def saveCart(self):
         self.session["cart"] = self.cart
@@ -64,14 +140,14 @@ class Cart:
             del self.cart[id]
             self.saveCart()
 
-    def substractItem(self, item):
-        id = str(item.id)
+    def substractItem(self, item, key):
+        id = key
         print(id)
-        if id in self.cart.keys():
-            self.cart[id]["quantity"] -= 1
-            self.cart[id]["price_acum"] -= float(item.price)
-            self.cart[id]["price_acum"] = round(self.cart[id]["price_acum"],2)
-            if self.cart[id]["quantity"] <= 0: self.delete(item) 
+        if str(id) in self.cart.keys():
+            self.cart[str(id)]["quantity"] -= 1
+            self.cart[str(id)]["price_acum"] -= float(item.price)
+            self.cart[str(id)]["price_acum"] = round(self.cart[str(id)]["price_acum"],2)
+            if self.cart[str(id)]["quantity"] <= 0: self.cart.pop(str(id))
             self.saveCart()
 
     def clean(self):
