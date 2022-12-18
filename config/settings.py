8888dd02f04b4db3ae587cd96a0fa1d1406210b3
EddieReynosoR <eduardo_3reynoso@hotmail.com>
2022-12-17 21:58:12 -0800

    Final Changes

--- config/settings.py
+++ config/settings.py
@@ -155,8 +155,8 @@ AUTHENTICATION_BACKENDS = (
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ibesporty@gmail.com'
-EMAIL_HOST_PASSWORD = 'whcpbogfkdewrbfa'
+EMAIL_HOST_USER = 'besportye@gmail.com'
+EMAIL_HOST_PASSWORD = 'zqwchjvauxobtwau'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = ''
