851403800d0dd69794233aff97c7b6c0f9e59ea8
EddieReynosoR <eduardo_3reynoso@hotmail.com>
2023-10-28 18:28:35 -0700

    Vercel Config

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7lul5bs0ws3o7g=fleqx^2*akigl23zg)ilz7vnmyl-p6kf+a(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
