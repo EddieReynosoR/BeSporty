62e9a7dddf90e70cf7b5874c193c2397942a7718
EddieReynosoR <eduardo_3reynoso@hotmail.com>
2023-12-08 15:53:17 -0800

    changes

--- config/urls.py
+++ config/urls.py
@@ -26,3 +26,5 @@ urlpatterns = [
     path('issues/', include('issues.urls')),
     path('items/', include('items.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STAITC_ROOT)
